0daa174fb57f6e067c9bce0a8f672157a9cf1cf5
Anklebiter87 <Anklebiter87@gmail.com>
2014-04-29 15:52:11 -0400

    new file:   C_Client.c 	new file:   C_Server.c

---
+++ C_Client.c
@@ -0,0 +1,57 @@
+﻿#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+int main(int argc, char*argv[])
+{
+    struct addrinfo hints, *ipinfo;
+    char* send_msg = "I connected to you";
+    int len = strlen(send_msg);
+    char recv_msg[100 + len];
+    int sent_bytes, recv_bytes;
+    int connect_fd;
+    int connectedtest;
+
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+
+    // sets the fields in the addrinfo structs
+    getaddrinfo("127.0.0.1", "3892", &hints, &ipinfo);
+    connect_fd = socket(ipinfo->ai_family, ipinfo->ai_socktype, ipinfo->ai_protocol);
+    if(connect_fd == -1){
+        printf("socket error\n");
+        return(1);
+    }
+
+
+    // Attempt to connect to server
+    connectedtest = connect(connect_fd, ipinfo->ai_addr, ipinfo->ai_addrlen);
+    if (connectedtest == -1){
+        printf("Failed to connect\n");
+        return(2);
+    }
+
+
+    // Connection successful; attempt to send & receive data
+    len = strlen(send_msg);
+    sent_bytes = send(connect_fd, send_msg, len, 0);
+    printf("You sent: %s\n", send_msg);
+    bzero(recv_msg, 100 + len);
+    recv_bytes = recv(connect_fd, recv_msg, sizeof recv_msg, 0);
+    printf("You received: %s\n", recv_msg);
+
+
+    // Close connection
+    close(connect_fd);
+
+
+    printf("You sent %d bytes\n", sent_bytes);
+    printf("You received %d bytes:\n", recv_bytes);
+    return 0;
+}
---
+++ C_Server.c
@@ -0,0 +1,72 @@
+﻿#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char*argv[])
+{
+    struct sockaddr_storage connectinfo;
+    socklen_t connectinfosize;
+    struct addrinfo hints, *ipinfo;
+    int sockfd;
+    int newsockfd;
+    int status;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    // removes need for hard-coding server IP address
+    hints.ai_flags = AI_PASSIVE;
+
+
+    // sets server port to 3892
+    // NULL IP address filled by getaddrinfo due to AI_PASSIVE flag
+    status = getaddrinfo(NULL, "3892", &hints, &ipinfo);
+
+
+    if(status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+
+    // Initializes the socket descriptor, binds the port, then listens
+    sockfd = socket(ipinfo->ai_family, ipinfo->ai_socktype, ipinfo->ai_protocol);
+    bind(sockfd, ipinfo->ai_addr,ipinfo->ai_addrlen);
+    listen(sockfd, 10);
+    connectinfosize = sizeof connectinfo;
+
+
+   // loop to keep looking for new connections
+    for(;;) {
+        // accepts connection on newsockfd; allows listen persistence of sockfd
+        newsockfd = accept(sockfd, (struct sockaddr *)&connectinfo, &connectinfosize);
+        int pid;
+        if( (pid=fork())== -1){
+            close(newsockfd);
+            continue;
+        }
+        else {
+            char recv_buf[100];
+            char send_buf[200];
+            bzero(recv_buf, 100);
+            bzero(send_buf, 200);
+            recv(newsockfd, recv_buf, sizeof recv_buf, 0);          
+            printf("You received: %s\n", recv_buf);
+            strncpy(send_buf, "You sent this to the server: ", 29);
+            strncat(send_buf, recv_buf, sizeof recv_buf);
+            send(newsockfd, send_buf, sizeof send_buf, 0);
+            printf("You sent: %s\n", send_buf);
+            close(newsockfd);
+        }
+    }
+
+
+    // closes the listener
+    close(sockfd);
+    freeaddrinfo(ipinfo);
+    return 0;
+}
